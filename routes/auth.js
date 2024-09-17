ce1acdba47d756e52be9cf0d15ffa2f0329841b1
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-09-16 21:33:35 -0300

    feat: add auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { login } = require("../controllers/api");
+const {validateFields} = require('../middleware/validateFields');
 
 const router = Router();
 
 router.post("/login",[
     check("email", "Email is required").isEmail(),
     check("password", "Password is required").not().isEmpty(),
+    validateFields
 ], login);
 
 module.exports = router;
