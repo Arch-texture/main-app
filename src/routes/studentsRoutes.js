65e0c7cef0d7b966bca6e4a43ebb6a488ce9d50f
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-12-19 22:32:14 -0300

    feat: add secondLastName field to create student route

--- src/routes/studentsRoutes.js
+++ src/routes/studentsRoutes.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { getStudents, createStudent } = require("../controllers/studentsController");
+const {
+  getStudents,
+  createStudent,
+} = require("../controllers/studentsController");
 const { validateFields } = require("../middleware/validateFieldsMiddleware");
 const { validateRole } = require("../middleware/validateRoleMiddleware");
 
@@ -9,10 +12,15 @@ const router = Router();
 router.get("/", [validateRole("logged")], getStudents);
 
 router.post(
-  "/create",
+  "/",
   [
     check("name").isString().withMessage("Name must be a string"),
-    check("lastName").isString().withMessage("Lastname must be a string"),
+    check("firstLastName")
+      .isString()
+      .withMessage("firstLastname must be a string"),
+    check("secondLastName")
+      .isString()
+      .withMessage("secondLastname must be a string"),
     check("email").isEmail().withMessage("Invalid email format"),
     validateFields,
   ],
